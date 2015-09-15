1da0a2071dab19f2ac443e5e04bf93ef16fb3b2a
F8Full <f8full@gmail.com>
2015-09-15 15:38:00 -0400

    Added MyLocation support
    - Using the fuseloctionprovider from Googlle Play Services
    
    TODO: add proper handling code for Google Play Services apk presence check

--- app/src/main/java/com/f8full/sample/directionsonmapv2withretrofit/DirectionsMapFragment.java
+++ app/src/main/java/com/f8full/sample/directionsonmapv2withretrofit/DirectionsMapFragment.java
@@ -1,22 +1,40 @@
 package com.f8full.sample.directionsonmapv2withretrofit;
 
 import android.app.Fragment;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.location.LocationListener;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class DirectionsMapFragment extends Fragment implements OnMapReadyCallback {
+public class DirectionsMapFragment extends Fragment implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener,
+        OnMapReadyCallback {
 
     private GoogleMap mMapInterface = null;
+    private GoogleApiClient mGoogleApiClient;
+
+    // These settings are the same as the settings for the map. They will in fact give you updates
+    // at the maximal rates currently possible.
+    private static final LocationRequest mREQUEST = LocationRequest.create()
+            .setInterval(5000)         // 5 seconds
+            .setFastestInterval(16)    // 16ms = 60fps
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
 
 
@@ -32,13 +50,58 @@ public class DirectionsMapFragment extends Fragment implements OnMapReadyCallbac
         if(mMapInterface == null)
             ((MapFragment) getActivity().getFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
 
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity().getApplicationContext())
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
 
 
         return inflatedView;
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
     public void onMapReady(GoogleMap googleMap) {
         mMapInterface = googleMap;
+        mMapInterface.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient,
+                mREQUEST,
+                this);  // LocationListener
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        //Nothing
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
