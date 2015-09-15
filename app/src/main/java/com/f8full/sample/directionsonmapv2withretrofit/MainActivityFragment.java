fc36498056f614fb6e81934090854a7a479ba558
F8Full <f8full@gmail.com>
2015-09-15 14:04:21 -0400

    Initial commit
    New blank activity with fragment project

---
+++ app/src/main/java/com/f8full/sample/directionsonmapv2withretrofit/MainActivityFragment.java
@@ -0,0 +1,23 @@
+package com.f8full.sample.directionsonmapv2withretrofit;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+}
